df1a72b1b9a3c3ce80f5e3c18d49b470b30d1aab
ronyshchenko <roma.onyshchenko@gmail.com>
2025-07-31 14:47:42 +0300

    Added task1 and task2

--- homework-solutions/js/hw-4/task-2.js
+++ homework-solutions/js/hw-4/task-2.js
@@ -14,3 +14,27 @@
   5. Проверить задание со следующими значениями в переменной age: 10, 17, 18, 19, 59, 60, 61
 
   */
+
+function checkAge(item) {
+  const minAge = 18;
+  const maxAge = 60;
+  let age;
+  age = item;
+
+  if ((typeof age) !== "number") {
+    console.log("Incorrect data type");
+    return;
+  } else if (age < minAge) {
+      console.log("You don't have access cause your age is " + age + " It's less then ");
+    } else if (age >= minAge && age < maxAge) {
+        console.log("Welcome  !");
+      } else if (age > maxAge) {
+        console.log("Keep calm and look Culture channel");
+        } else {
+          console.log("Technical work");
+          }
+}
+
+[10, 17, 18, 19, 59, 60, 61].forEach(item => checkAge(item));
+
+
