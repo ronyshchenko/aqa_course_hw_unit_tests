497002756dd722255d15710d8f9619caabe4b867
ronyshchenko <roma.onyshchenko@gmail.com>
2025-08-05 17:11:31 +0300

    solution of task 1, task 2, task 3

--- homework-solutions/js/hw-5/task-1.js
+++ homework-solutions/js/hw-5/task-1.js
@@ -6,6 +6,14 @@
 
 let evenNumbersResult = '';
 
+for (let i = 10; i >= 0; i = i - 2) {
+  if (i <= 10 && i > 0) {
+    evenNumbersResult = evenNumbersResult + i + '-';
+  } else {
+    evenNumbersResult = evenNumbersResult + i;
+  }
+}
+
 /**
  * Создать строку из 5 строк с увеличивающимся количеством смайликов ":)".
  * Переменная для результата `smilePatternResult` уже создана и содержит пустую строку.
@@ -19,6 +27,8 @@ let evenNumbersResult = '';
 
 let smilePatternResult = '';
 
+smilePatternResult = smilePatternResult + ':)\n' + ':):)\n' + ':):):)\n' + ':):):):)\n' + ':):):):):)';
+
 /**
  * Заменить все пробелы в переменной text на "1".
  * Переменная для результата `replaceSpacesWithOneResult` уже создана и содержит пустую строку.
@@ -26,5 +36,7 @@ let smilePatternResult = '';
  */
 const text = 'Hello! I am a JS student!';
 let replaceSpacesWithOneResult = '';
+
+replaceSpacesWithOneResult = text.replace(/ /g, '1');
 
 export { evenNumbersResult, smilePatternResult, replaceSpacesWithOneResult };
--- homework-solutions/js/hw-5/task-3.js
+++ homework-solutions/js/hw-5/task-3.js
@@ -7,4 +7,35 @@ const word = 'hello';
 
 let vowelsAndConsonantsResult = '';
 
+vowels = ['A', 'E', 'I', 'O', 'U', 'Y'];
+consonants = ['B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z'];
+
+let countOfvowels = 0;
+let countOfconsonants = 0;
+
+function countOccurrences(str, char) {
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === char) {
+      count++;
+    }
+  }
+  return count;
+}
+
+str = word;
+str = str.toUpperCase();
+
+consonants.forEach((element) => {
+  countOfconsonants = countOfconsonants + countOccurrences(str, element);
+});
+
+vowels.forEach((element) => {
+  countOfvowels = countOfvowels + countOccurrences(str, element);
+});
+
+vowelsAndConsonantsResult = `${word} contains ${countOfvowels} vowels and ${countOfconsonants} consonants`;
+
+//console.log(vowelsAndConsonantsResult);
+
 export { vowelsAndConsonantsResult };
