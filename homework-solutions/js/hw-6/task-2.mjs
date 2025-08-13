d914bc4de4536cf3db7b3e4e79bb7c14cc058f51
ronyshchenko <roma.onyshchenko@gmail.com>
2025-08-14 00:18:48 +0300

    solution HW-6

--- homework-solutions/js/hw-6/task-2.mjs
+++ homework-solutions/js/hw-6/task-2.mjs
@@ -13,7 +13,30 @@
   const myPizzasT2 = ['Peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawai'];
 */
 
-let resultUnique;
+const competitorPizzas = ['Peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawai']
+const myPizzasT1 = ['Peperoni', 'Margherita', 'Diablo', 'Vegetarian'];
+const myPizzasT2 = ['Peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawai'];
+
+let resultUnique = [];
+let resultUnique1 = [];
 let resultNull;
+
+for(const elem of myPizzasT1) {
+  if (!competitorPizzas.includes(elem)) {
+    resultUnique.push(elem)
+  }
+}
+
+
+for(const elem of myPizzasT2 ) {
+  if (!competitorPizzas.includes(elem)) {
+    resultUnique1.push(elem)
+  }
+}
+
+if (resultUnique1.length === 0) {
+  resultNull = null;
+}
+
 
 export { resultNull, resultUnique };
