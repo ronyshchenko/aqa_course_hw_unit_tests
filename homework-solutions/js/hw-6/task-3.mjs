545e4cba647ae50012fa098c2930fe5c573bc0a9
ronyshchenko <roma.onyshchenko@gmail.com>
2025-08-19 07:58:37 +0300

    start solution of HW-7

--- homework-solutions/js/hw-6/task-3.mjs
+++ homework-solutions/js/hw-6/task-3.mjs
@@ -10,11 +10,17 @@ let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 2, 4, 6, 8, 10, 1, 3, 5, 7, 9];
 
 let unique;
 
-for (let i = 0; i < array.length; ) {
-  if (array.indexOf(array[i], i + 1) !== -1) {
-    array.splice(array.indexOf(array[i], i + 1), 1);
-  } else i++;
+// for (let i = 0; i < array.length; ) {
+//   if (array.indexOf(array[i], i + 1) !== -1) {
+//     array.splice(array.indexOf(array[i], i + 1), 1);
+//   } else i++;
+// }
+
+for (let i = 0; i < array.length; i++) {
+  let duplicateNumberIndex = array.indexOf(array[i], i + 1);
+  if (duplicateNumberIndex !== -1)  array.splice(duplicateNumberIndex, 1);
 }
 
 unique = [...array];
+console.log(unique);
 export { unique };
--- homework-solutions/js/hw-7/task-1.mjs
+++ homework-solutions/js/hw-7/task-1.mjs
@@ -5,18 +5,75 @@
   - Например: mergeArrays([1,2], [3,4], [5,6]) // [1,2,3,4,5,6]
   - Решить с использованием Spread operator
 */
-function mergeArrays() {
-  // Ваш код
+function mergeArrays(...args) {
+  let resultArrays = [];
+  for(let elem of args) {
+    resultArrays.push(...elem)
+  }
+
+  return resultArrays;
 }
+
+//console.log(mergeArrays([1,2], [3,4], [5,6]));
+
+
+
 /*
   2. Devide by _
     - Написать функцию, которая преобразует любое предложение в вот_Такой_Вот_Вид и возвращает его. 
     - Первое слово должно начинаться с буквы в нижнем регистре, у остальных -  верхнем. 
     - Пример: I am super engineer => i_Am_Super_Engineer
   */
+let arrayWords = [];
+let arrayWords1 = [];
+let arraySymbols = [];
+//let arraySymbols1 = [];
+let stringFromArrayWords;
+let stringFromArraySymbols;
+let resultArr = [];
+
+let x;
 function devideBy(sentence) {
-  // Ваш код
+  if(sentence.length === '') {
+    return sentence;
+  }
+  arrayWords = sentence.split(' ');
+  //console.log(arrayWords)
+  for(let i=0; i<arrayWords.length; i++ ) {
+    let arraySymbols1 = [];
+    if (arrayWords[i].length !== 0){ 
+      
+      arraySymbols = arrayWords[i].split('');
+      //console.log(arraySymbols);
+      
+      for(let el of arraySymbols) {
+        //console.log(el);
+        x= el.toLowerCase();
+         arraySymbols1.push(x);
+         //console.log(x);
+      }
+      //console.log(arraySymbols1);
+
+    if(i==0) {
+      arraySymbols1[0] = arraySymbols[0].toLowerCase();
+    } else {
+        arraySymbols1[0] = arraySymbols[0].toUpperCase();
+    }
+
+    stringFromArraySymbols = arraySymbols1.join('');
+    resultArr.push(stringFromArraySymbols);
+    }
+    
+  }
+  stringFromArrayWords = resultArr.join('_');
+  return stringFromArrayWords;
 }
+
+let res = devideBy('I    am    an     engineer')
+console.log(res);
+
+
+
 /*
   3. Фибаначчи
     - Напишите функцию fibonacci(n), возвращающую энное число Фибоначчи
@@ -26,7 +83,27 @@ function devideBy(sentence) {
     - Например fibonacci(8) //21
   */
 function fibonacci(n) {
-  // Ваш код
+
+  let firstFibonachi = 0;
+  let secondFibonachi = 1;
+  let nextFibonachi;
+
+   if(n == 0) {
+      return firstFibonachi;
+    } else if (n == 1) {
+      return secondFibonachi;
+    } else {
+        for(let i=2; i<=n;i++){
+          nextFibonachi=firstFibonachi + secondFibonachi;
+          firstFibonachi = secondFibonachi;
+          secondFibonachi = nextFibonachi;
+        }
+      }
+  return nextFibonachi;
 }
+
+let fibonachiFromN = fibonacci(8)
+
+//console.log(fibonachiFromN)
 
 export { mergeArrays, fibonacci, devideBy };
