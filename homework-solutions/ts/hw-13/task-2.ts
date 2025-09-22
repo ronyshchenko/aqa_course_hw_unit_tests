164d93d5205d738cacee133f1010970acca5f503
ronyshchenko <roma.onyshchenko@gmail.com>
2025-09-22 16:45:09 +0300

    HW-13 resolve

---
+++ homework-solutions/ts/hw-13/task-2.ts
@@ -0,0 +1,21 @@
+// Создайте функцию validatePassword, которая принимает строку (пароль) и возвращает true, 
+// если пароль соответствует следующим правилам:
+//   - Пароль должен содержать хотя бы одну заглавную букву.
+//   - Пароль должен содержать хотя бы одну букву в нижнем регистре.
+//   - Пароль должен содержать хотя бы одну цифру.
+//   - Пароль должен быть не менее 8 символов.
+//   - Пароль не должен состоять из одних пробелов
+// Функция должна возвращать false, если хотя бы одно из условий не выполнено.
+
+
+function validatePassword(password: string): Boolean {
+    let isCorectPassword: Boolean;
+    isCorectPassword = password.length >= 8 &&
+        (password.toLowerCase() != password) &&
+        (password != password.toUpperCase()) &&
+        (password.trim().length != 0) &&
+        (/\d/.test(password));;
+    return isCorectPassword;
+}
+
+console.log(validatePassword('Dobrolubova22'));
